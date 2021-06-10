402b9a76e03a59bb3c807b4ba4a03ca4ce9ddc80
LuongMinhDuong <Duong40541@gmail.com>
2021-06-10 13:47:12 +0700

    first commit

---
+++ src/advance/dev/Student.java
@@ -0,0 +1,19 @@
+package advance.dev;
+
+public class Student {
+	String TEN;
+	int Tuoi;
+	String DiaChi;
+	String SoDienThoai;
+	double DiemTrungBinh;
+	public Student(String tEN, int tuoi, String diaChi, String soDienThoai, double diemTrungBinh) {
+		super();
+		TEN = tEN;
+		Tuoi = tuoi;
+		DiaChi = diaChi;
+		SoDienThoai = soDienThoai;
+		DiemTrungBinh = diemTrungBinh;
+	}
+	
+	
+}
